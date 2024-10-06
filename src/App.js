da51e5cfc0e94c20b33523b947419077887a8c54
RosaMartinez23 <20161251@itoaxaca.edu.mx>
2024-10-05 20:17:16 -0600

    Menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React from 'react';
 import './App.css';
 import Menu from './components/menu/menu';
+
+const items = [
+  {
+    name: "Freelance",
+    color: "#f44336",
+    href: "#"
+  },
+  {
+    name: "Design",
+    color: "#e91e63",
+    href: "#"
+  },
+  {
+    name: "Director",
+    color: "#9c27b0",
+    href: "#"
+  },
+  {
+    name: "Experience",
+    color: "#673ab7",
+    href: "#"
+  },
+  {
+    name: "Interface",
+    color: "#3f51b5",
+    href: "#"
+  }
+];
+
 function App() {
   return (
-    
     <div className="App">
+      {/* Asegúrate de pasar 'items' como prop */}
+      <Menu items={items} />
     </div>
   );
 }
